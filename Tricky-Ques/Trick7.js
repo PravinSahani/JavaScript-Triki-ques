2e8aeec28d70e32183e2723a5ae0d7c18a96cf8c
PravinSahani <pravinkumarsahani7@gmail.com>
2022-05-22 01:17:53 +0530

    magic with obj and forEach

---
+++ Tricky-Ques/Trick7.js
@@ -0,0 +1,7 @@
+var d ={};
+
+['pravin','pankaj'].forEach((k)=>{
+    d[k] = undefined;//It sets the key as pravin and pankaj to the object d and values for them is undefined
+});
+
+console.log(d);

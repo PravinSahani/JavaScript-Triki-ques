145f1da17e195139548856b441eb1e027d41336e
PravinSahani <pravinkumarsahani7@gmail.com>
2022-05-21 15:32:27 +0530

    loop with Let Var Const

---
+++ ForLoopWithVarLetConst/index.js
@@ -0,0 +1,14 @@
+for(var i =0; i<3; i++){
+
+}
+console.log(i); //Prints i =3
+
+for(let i=0; i<3; i++){
+
+}
+console.log(i); //errors happen that i is not defined 
+
+for(const i =0; i<3; i++){
+
+}
+console.log(i);// Error: Assignment to constant variable.
